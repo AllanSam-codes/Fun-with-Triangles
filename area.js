4931196a37f3219c55223875db4c2a373c30e3a3
AllanSam <93815162+AllanSam-codes@users.noreply.github.com>
2022-10-14 21:17:44 +0530

    added some styles

--- area.js
+++ area.js
@@ -1,13 +1,16 @@
-const area = document.querySelectorAll(".input-area");
+//const area = document.querySelectorAll(".input-area");
+const inputBase = document.querySelector("#base");
+const inputHeight = document.querySelector("#height")
 const areaBtn = document.querySelector("#area-btn");
 const output = document.querySelector("#output");
 
+//console.log(inputBase.value, inputHeight.value)
+//console.log(areaBtn, output)
 
-//console.log(area, areaBtn, output)
-function calculateAreaOfTriangle(a, b) {
-    const sumOfSquares = a * a + b * b
-        //console.log(sumOfSquares);
-    return sumOfSquares
+function calculateAreaOfTriangle() {
+    //console.log("Calculate area called", inputBase.value, inputHeight.value);
+    var area = (0.5) * inputBase.value * inputHeight.value;
+    output.innerText = "The area of a triangle is " + area;
 }
 
 areaBtn.addEventListener('click', calculateAreaOfTriangle);
